7823838bc196a6d06610177fd32556a4cbaef838
samuvel <samuvelsherbin15@gmail.com>
2026-09-22 17:36:33 +0530

    update

---
+++ Javascript/Day1/Assets/js/index.js
@@ -0,0 +1,26 @@
+var username = "print"
+console.log(username);
+
+
+var age ="30"
+console.log(age);
+
+var product="true"
+console.log(product);
+
+ var city;
+ console.log(city);
+
+ var character =null
+ console.log(character);
+
+
+
+ var number ="200"
+ console.log(number);
+ 
+ 
+ 
+
+
+
