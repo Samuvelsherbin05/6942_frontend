a5aca104b488f0d782c5ff4eef1682c691380743
samuvel <samuvelsherbin15@gmail.com>
2026-09-22 23:46:44 +0530

    update

---
+++ Javascript/Day2/Assets/js/index.js
@@ -0,0 +1,124 @@
+var name ="lokesh"
+console.log(name);
+
+var age ="45"
+console.log(age);
+
+var course ="fullstack"
+console.log(course);
+
+var language ="spanish"
+console.log(language);
+
+var subject ="computer"
+console.log(subject);
+
+var hobby="football"
+console.log(hobby);
+
+var food ="biriyani"
+console.log(food);
+
+
+var movie="kushi"
+console.log(movie);
+
+
+var singer ="sidsree ram"
+console.log(singer);
+
+
+var sports ="cricket"
+console.log(sports);
+
+
+var age =23;
+console.log(age);
+
+var marks =90;
+console.log(marks);
+
+
+
+var salary =50000;
+console.log(salary);
+
+var price =657;
+console.log(price);
+
+var count =10;
+console.log(count);
+
+
+
+var score =250;
+console.log(score);
+
+var year =2027;
+console.log(year);
+
+
+
+var quantity =3;
+console.log(quantity);
+
+
+var height =160;
+console.log(height);
+
+
+
+var percentage =50;
+console.log(percentage);
+
+var isStudent =true;
+console.log(isStudent);
+
+var isLoggedin =false;
+console.log(isLoggedin);
+
+var isActive =false;
+console.log(isActive);
+
+var isCompleted =true;
+console.log(isCompleted);
+
+var isAvailable =true;
+console.log(isAvailable);
+
+var username =null;
+console.log(username);
+
+var userage =null;
+console.log(userage);
+
+var active =null;
+console.log(active);
+
+var city;
+console.log(city)
+
+var product;
+console.log(product);
+
+var result;
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
