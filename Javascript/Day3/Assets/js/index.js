e361329f9cff1007042d05a84ce94ac1c5b0df26
samuvel <samuvelsherbin15@gmail.com>
2026-09-24 00:10:29 +0530

    update

---
+++ Javascript/Day3/Assets/js/index.js
@@ -0,0 +1,85 @@
+
+
+let shirtprice =1200
+console.log(shirtprice);
+
+shirtprice *= 3
+console.log(shirtprice);
+
+shirtprice += 100
+console.log(shirtprice);
+
+
+
+let employeesalary =30000
+
+employeesalary +=5000
+console.log(employeesalary);
+
+employeesalary -=2000
+console.log(employeesalary);
+
+
+
+let startingbalance =20000
+
+startingbalance +=5000
+console.log(startingbalance);
+
+startingbalance -=3000
+console.log(startingbalance);
+
+
+
+
+let Tamil = 80
+
+ let English = 75
+
+ let Maths = 90
+
+ let Science = 85
+
+ let Computer = 95
+
+let totalmarks =Tamil+English+Maths+Science+Computer
+console.log(totalmarks);
+
+ Averagemarks=totalmarks/5
+console.log(Averagemarks);
+
+
+
+
+
+let studentnumber =20;
+
+
+studentnumber +=10;
+console.log(studentnumber);
+
+
+studentnumber *=2;
+console.log(studentnumber);
+
+studentnumber -=20;
+console.log(studentnumber);
+
+studentnumber /=4;
+console.log(studentnumber);
+
+studentnumber %=3;
+console.log(studentnumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/Day3/Assets/js/index1.js
@@ -0,0 +1,153 @@
+let valuedata ="print"
+console.log(valuedata);
+
+valuedata +=" "
+console.log(valuedata );
+
+
+valuedata +="This is Js"
+console.log(valuedata);
+
+let studentname ="deepak"
+console.log(studentname);
+
+studentname +="-"
+console.log(studentname);
+
+studentname += "kumar"
+console.log(studentname);
+
+let coursename ="fullstack"
+console.log(coursename);
+
+
+coursename +=" "
+console.log(coursename);
+
+
+coursename +="development"
+console.log(coursename);
+
+let cityname ="pulicat"
+console.log(cityname);
+
+cityname +=" "
+console.log(cityname);
+
+
+cityname +="601205"
+console.log(cityname);
+
+let favoritefood ="chicken"
+console.log(favoritefood);
+
+favoritefood +="biriyani"
+console.log(favoritefood);
+
+let favoritesports ="cricket"
+console.log(favoritesports);
+
+favoritesports +=" "
+console.log(favoritesports);
+
+
+favoritesports +="football"
+console.log(favoritesports);
+
+let bestsinger ="spb"
+console.log(bestsinger);
+
+bestsinger +=" "
+console.log(bestsinger);
+
+bestsinger +="Gvprakash"
+console.log(bestsinger);
+
+let collegename ="loyola"
+console.log(collegename);
+
+collegename +=" "
+console.log(collegename);
+
+collegename +="SRM"
+console.log(collegename);
+
+
+let phonetype ="android"
+console.log(phonetype);
+
+phonetype +=" -"
+console.log(phonetype);
+
+phonetype +="apple"
+console.log(phonetype);
+
+let countryname ="india"
+console.log(countryname);
+
+countryname +=","
+console.log(countryname);
+
+countryname +="Amrica"
+console.log(countryname);
+
+countryname +=","
+console.log(countryname);
+
+countryname +="england"
+console.log(countryname);
+
+
+let age ="20"
+console.log(age);
+
+age ="32"
+console.log(age);
+
+let phonenumber ="3265478765"
+console.log(phonenumber);
+
+phonenumber ="7646539808"
+console.log(phonenumber);
+
+let price ="1500"
+console.log(price);
+
+price ="1499"
+console.log(price);
+
+let size ="35"
+console.log(size);
+
+size ="40"
+console.log(size);
+
+
+let studentid ="2301111033001"
+console.log(studentid);
+
+studentid ="2310111033012"
+console.log(studentid);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
